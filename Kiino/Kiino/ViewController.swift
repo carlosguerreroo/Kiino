e8ca9fa469de20ccddcd659871225dfc061e8e4b
Carlos Omana <araogaraog@gmail.com>
2015-03-09 19:01:37 -0600

    Adds empty text validation

--- Kiino/Kiino/ViewController.swift
+++ Kiino/Kiino/ViewController.swift
@@ -28,7 +28,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldShouldReturn(textField: UITextField!) -> Bool {
         textField.resignFirstResponder()
+        self.searcher.text = nil
         return true
+    }
+    
+    func validateSearch () -> Bool {
+    
+        return (self.searcher.text == "") ? false : true
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
+        
+        if (identifier! == "searching" && !self.validateSearch()) {
+            self.displayErrorMessage()
+        }
+        
+        return true
+    }
+    
+    func displayErrorMessage() {
+        
+        
+        var alert = UIAlertController(title: nil, message: "Are you sure about that?", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 
 }
