b7bdd832768cac68a794f8577e4e2337c2e59d98
Carlos Omana <araogaraog@gmail.com>
2015-03-12 17:02:54 -0600

    Adds side bar to search view controller

--- Kiino/Kiino/AppDelegate.swift
+++ Kiino/Kiino/AppDelegate.swift
@@ -18,28 +18,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         Parse.setApplicationId("Pqij1heFb72OBZ2l4WjPqqan565JWmvyqAAoH4wr", clientKey: "okkwKhfUbVHGZAAMt9ksn0qL0ivpPtcAlPHTy2bw")
         var object = PFObject(className: "testDataClass")
-//        object.addObject("iOSBlog", forKey: "websiteUrl")
-//        object.addObject("Five", forKey: "websiteRating")
-//        object.saveInBackgroundWithBlock(nil)
         PFFacebookUtils.initializeFacebook()
-        
         PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)
+        
+        if PFUser.currentUser() != nil{
+            let menuViewController = self.window?.rootViewController?.storyboard?.instantiateViewControllerWithIdentifier("NavigationSearchController") as NavigationSearchController
+            self.window?.rootViewController? = menuViewController
+        }
         
         return true
     }
 
     func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
     
     func application(application: UIApplication,
@@ -51,7 +47,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func resetApp(){
-        self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("LoginViewController") as LoginViewController
+        self.window?.rootViewController =
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("LoginViewController") as LoginViewController
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
@@ -59,7 +56,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
 
--- Kiino/Kiino/ViewController.swift
+++ Kiino/Kiino/ViewController.swift
@@ -8,19 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
-
-    @IBOutlet weak var searcher: UITextField!
+class ViewController: UIViewController, UITextFieldDelegate, UIGestureRecognizerDelegate {
     
-    @IBAction func fbLogout(sender: AnyObject) {
-        
-    }
+    @IBOutlet weak var searcher: UITextField!
+    @IBOutlet weak var sideBar: UIView!
+
+    var screenEdgeRecognizer: UIScreenEdgePanGestureRecognizer!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searcher.delegate = self
-
+        self.addGestureRecognizerToView()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -35,6 +34,38 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.resignFirstResponder()
         self.searcher.text = nil
         return true
+    }
+    
+    func addGestureRecognizerToView(){
+        self.screenEdgeRecognizer = UIScreenEdgePanGestureRecognizer(target: self,
+            action: "showSideBar:")
+        self.screenEdgeRecognizer.edges = .Left
+        self.view.addGestureRecognizer(screenEdgeRecognizer)
+    }
+    
+    func showSideBar(sender: UIScreenEdgePanGestureRecognizer){
+
+        var translate = sender.translationInView(self.view)
+        var newFrame = self.sideBar.frame
+        newFrame.origin.x += translate.x
+        newFrame.origin.x += self.sideBar.frame.width
+        
+        if newFrame.origin.x >= self.sideBar.frame.width {
+            return
+        }
+        
+        self.sideBar.frame.origin.x += translate.x
+        
+        
+        if(sender.state == UIGestureRecognizerState.Ended){
+            if (self.sideBar.frame.origin.x >= (self.sideBar.frame.width/2.0 * -1)) {
+                self.handleSideBar(0.0)
+            }else{
+                self.handleSideBar(self.sideBar.frame.width * -1)
+            }
+        } else {
+            sender.setTranslation(CGPointMake(0,0), inView: self.sideBar)
+        }
     }
     
     func validateSearch () -> Bool {
@@ -53,11 +84,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func displayErrorMessage() {
         
-        
-        var alert = UIAlertController(title: nil, message: "Are you sure about that?", preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        var alert =
+            UIAlertController(title: nil, message: "Are you sure about that?",
+                preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok",
+            style: UIAlertActionStyle.Default, handler: nil))
         self.presentViewController(alert, animated: true, completion: nil)
     }
-
+    
+    func handleSideBar(x:CGFloat){
+        UIView.animateWithDuration(0.5,
+            delay: 0,
+            usingSpringWithDamping: 0.8,
+            initialSpringVelocity: 0,
+            options: .CurveEaseInOut,
+            animations: {
+                self.sideBar.frame = CGRectMake(x, 0.0, self.sideBar.frame.width, self.sideBar.frame.height)
+            }, completion: nil)
+        
+    }
 }
-
