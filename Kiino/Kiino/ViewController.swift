3e582807c770feddd250d0f277d92152535a9c2e
Carlos Omana <araogaraog@gmail.com>
2015-04-08 18:59:32 -0500

    Adds twitter login handler

--- Kiino/Kiino/ViewController.swift
+++ Kiino/Kiino/ViewController.swift
@@ -140,6 +140,14 @@ class ViewController: UIViewController, UITextFieldDelegate, UIGestureRecognizer
     
     @IBAction func login(sender: AnyObject) {
         
+        if !PFTwitterUtils.isLinkedWithUser(PFUser.currentUser()) {
+            PFTwitterUtils.linkUser(PFUser.currentUser(), {
+                (succeeded: Bool!, error: NSError!) -> Void in
+                if PFTwitterUtils.isLinkedWithUser(PFUser.currentUser()) {
+                    println("Woohoo, user logged in with Twitter!")
+                }
+            })
+        }
     }
     
 }
