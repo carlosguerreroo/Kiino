d6b351be05d3c94a802a1a6f39f83eb5fadd1058
Carlos Omana <araogaraog@gmail.com>
2015-05-15 16:06:39 -0500

    Adds NewCollectionViewCell

---
+++ Kiino/Kiino/NewsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewsCollectionViewCell.swift
+//  Kiino
+//
+//  Created by Carlos Guerrero on 5/15/15.
+//  Copyright (c) 2015 demo. All rights reserved.
+//
+
+import UIKit
+
+class NewsCollectionViewCell: UICollectionViewCell {
+    
+}
